6bda818f5a30f79b73fc41a7f0c86ab1d242d2d5
lucadruda <ludruda@microsoft.com>
2021-06-25 17:58:29 +0200

    increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-version = "1.1.0"
+version = "1.1.1"
 
 setuptools.setup(
     name='iotc',
@@ -24,10 +24,8 @@ setuptools.setup(
     classifiers=[
       'License :: OSI Approved :: MIT License',
       'Programming Language :: Python',
-      'Programming Language :: Python :: 2',
-      'Programming Language :: Python :: 2.7',
       'Programming Language :: Python :: 3',
-      'Programming Language :: Python :: 3.8',
+      'Programming Language :: Python :: 3.7+'
     ],
     include_package_data=True,
     install_requires=["azure-iot-device"]
